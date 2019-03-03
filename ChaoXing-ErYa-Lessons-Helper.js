6076926e4e95d71889513b781add82cf57d78de7
HollowMan6 <jiangsl18@lzu.edu.cn>
2019-03-03 11:09:59 +0800

    Initial Commit

---
+++ ChaoXing-ErYa-Lessons-Helper.js
@@ -0,0 +1,103 @@
+﻿// ==UserScript==
+// @icon            https://ss0.baidu.com/73t1bjeh1BF3odCf/it/u=3507160010,2121752339&fm=77&s=1947387E66836BF52423D3FB0300A01E
+// @name                        超星尔雅课程助手
+// @description                课程自动切换，自动静音播放（默认静音），播放线路切换（手动配置参数）
+// @supportURL      https://github.com/HollowMan6/ChaoXing-ErYa-Lessons-Helper
+// @match                        *://*.chaoxing.com/mycourse/*
+// @requier         https://mooc1-1.chaoxing.com/js/build/jquery.js
+// ==/UserScript==
+
+//按钮参数设置
+var fa = $("body");
+var btn = $("<li></li>");
+var json = {
+    "background": "#DC143C",
+    "height": "16px",
+    "padding": "5px",
+    "z-index": 999999,
+    "cursor": "pointer",
+    "top": "300px",
+    "right": "120px",
+    "position": "fixed"
+};
+btn.css(json);
+btn.html("<span id='lfsenior'>点击开启自动播放模式</span>");
+fa.append(btn);
+
+btn.click(function () {
+    var n = 2, m = 1, errors = 0;
+    setInterval(function () {
+        if (m == 1) {
+            //切换到视频标签
+            try {
+                let blk = document.getElementById("dct1");
+                if (blk.title == "视频") {
+                    blk.click();
+                }
+                else {
+                    document.getElementById("dct2").click();
+                }
+            }
+            catch (err) { console.warn(err.message); }
+            m -= 1;
+        }
+        let tmp;
+        let playline = $("iframe").contents().find("iframe").contents().find("li.vjs-menu-item.vjs-selected");
+        //获取iframe
+        var video = $("iframe").contents().find("iframe").contents();
+        //播放函数
+        var play = function () {
+            video.find("#video > button").click();
+            var jy = video.find("#video > div.vjs-control-bar > div.vjs-volume-panel.vjs-control.vjs-volume-panel-vertical > button");
+            if (jy.attr("title") != "取消静音") {
+                jy.click()
+            }
+        }
+        //如果正在加载
+        var load = video.find("#loading");
+        if (load.css("visibility") != "hidden") {
+            return;
+        }
+        //获取当前进度
+        var spans = video.find("#video > div.vjs-control-bar > div.vjs-progress-control.vjs-control > div").attr("aria-valuenow");
+        //切换线路
+        try {
+            if (n > 0) {
+                playline[1].nextElementSibling.click();
+            }
+            n -= 1;
+        }
+        catch (err) { errors += 1; }
+        //防止切换线路多次不成功导致的问题
+        if (errors >= 30) {
+            n = 0;
+        }
+        // 如果还没播放完
+        if (spans != 100) {
+            play();
+        }
+        //如果播放结束
+        if (spans == 100) {
+            //跳转到下一页
+            try {
+                tmp = $("h4.currents")[0].parentElement.parentElement.nextElementSibling.children[0];
+            }
+            catch (err) { tmp = $("h4.currents")[0].parentElement.parentElement.parentElement.nextElementSibling.children[0]; }
+            if (tmp.hasAttribute('href')) {
+                if (tmp.href != "javascript:void") {
+                    tmp.children[0].click();
+                }
+            }
+            else {
+                tmp = tmp.parentElement.children[1].children[0];
+                if (tmp.href != "javascript:void") {
+                    tmp.children[0].click();
+                }
+            }
+            //参数重置
+            n = 2, m = 1, errors = 0;
+        }
+        //显示播放进度
+        $("#lfsenior").html("自动模式已开启,本章进度:" + spans + "%");
+    }, 100);
+});
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# ChaoXing-ErYa-Lessons-Helper
-ChaoXing ErYa Lessons Helper（超星尔雅课程助手）
+# ChaoXing ErYa Lessons Helper（超星尔雅课程助手）
+
+本脚本适用于超星、尔雅慕课。若你的课程平台是智慧树，或者不会使用脚本，请参考[这篇文章](http://www.cnblogs.com/yinbiao/p/8721391.html)
+
+开启时只需点击按钮。如果想停止请刷新页面。
+
+如果你觉得验证码或视频中的弹题很讨厌，请尝试也建议向兼容 AdBlock Plus 语法的广告过滤扩展添加这条自定义规则：
+
+---
+\*.chaoxing.com/richvideo/initdatawithviewer\*
+
+---
+
+
+## 功能
+
+支持 课程自动切换，自动静音播放（默认静音），播放线路切换（需手动配置参数）;
+
+若想要更多功能，如 自动答题，5秒内一节课 （都是高危行为）的，请[参考这里](https://poxiaobbs.com/thread-3268-1-1.html)（未测试）;
+
+已知问题：已经显示完成的视频将无法自动切换线路。
+
+## 配置播放线路
+
+建议在谷歌浏览器下F12进入开发者模式，然后切换到console，在视频已经加载的前提下，首先输入以下命令：
+
+---
+let playline = $("iframe").contents().find("iframe").contents().find("li.vjs-menu-item.vjs-selected");
+
+---
+然后将下面命令的变量i从0递增，进行尝试，当看到显示了线路名称，记下i，并对脚本对应的地方进行修改：
+
+---
+playline[1].nextElementSibling.click();
+
+---
+
+如果不想切换线路，则可以将“切换线路”和“防止切换线路多次不成功导致的问题”注释包括的部分进行清理，并一并清理变量errors和n;
+
+## 声明：将本脚本运用于任何不正当途径所产生的不良后果，本人将不承担任何责任。
+
+因为时间有限，所以脚本做得有些简单，很有可能不太稳定，欢迎大家进行完善。
+
+测试时间：2019年3月2日
